6021f1ba578258509046f956da3086a6f848d46c
MateiG <matei.gardea@gmail.com>
2023-06-17 19:31:22 -0400

    added wsgi entrypoint

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def insight():
     return jsonify({'prediction': prediction})
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
