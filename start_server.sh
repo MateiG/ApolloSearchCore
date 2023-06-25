9c028381167fbef9427e070a24f3d617667d5888
MateiG <matei.gardea@gmail.com>
2023-06-25 19:03:39 +0000

    start and stop server scripts

--- start_server.sh
+++ start_server.sh
@@ -4,6 +4,9 @@ cd ~/ApolloSearchCore/
 
 source venv/bin/activate
 
+export GOOGLE_APPLICATION_CREDENTIALS="credentials/apollosearch-bebb5b92d946.json"
+export OPENAI_API_KEY="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"
+
 nohup gunicorn -w 4 -b 127.0.0.1:8000 wsgi:app > output.log 2>&1 &
 
 echo "Gunicorn server started"
