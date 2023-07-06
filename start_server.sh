90a84b9906a68660488d8811286f8e7ab9e56be2
MateiG <matei.gardea@gmail.com>
2023-07-06 23:36:49 +0000

    changed gunicorn timeout

--- start_server.sh
+++ start_server.sh
@@ -8,6 +8,6 @@ export GOOGLE_APPLICATION_CREDENTIALS="credentials/apollosearch-bebb5b92d946.jso
 export OPENAI_API_KEY="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"
 export MIXPANEL_KEY="091f7f4f16d98b2155901f950b488c1b"
 
-nohup gunicorn -w 4 -b 127.0.0.1:8000 wsgi:app > output.log 2>&1 &
+nohup gunicorn wsgi:app --workers 4 --bind 127.0.0.1:8000 --timeout 120  > output.log 2>&1 &
 
 echo "Gunicorn server started"
